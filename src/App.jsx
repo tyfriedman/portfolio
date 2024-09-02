c7a4383b45252b733987b1f841769dfd4a66e653
Ty Friedman <tyfriedman@pm.me>
2024-09-02 13:18:21 -0400

    updated site

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
+        <h1>Ty Friedman</h1>
       </header>
     </div>
   );
