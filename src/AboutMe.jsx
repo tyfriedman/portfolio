fef8268f6eae557d6c83b30598127364f3163c92
Ty Friedman <tyfriedman@pm.me>
2024-09-12 12:06:16 -0400

    added photo to about me page

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import pommelhorse from './assets/pommel.jpeg'
 import './AboutMe.css'; // Optionally, create a separate CSS file for About Me page styling
+
 
 const AboutMe = () => {
   return (
     <div className="about-me-page">
       <h1>About Me</h1>
-      {/* Add your about me content here */}
+      <img src={pommelhorse} alt="Pommel Horse" className="gymnastics-photo" />
+        <div>
+        </div>
     </div>
   );
 };
